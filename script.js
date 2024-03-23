b33c848a5f42f519d25006f278743fbd205ad625
Rishabh Singh <52191143+rishabh0085@users.noreply.github.com>
2024-03-23 18:53:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function toCase(text) {
-  // write your code here
+  if (!text) return '-'; // Return '-' for empty strings
+  
+  const lowerCase = text.toLowerCase();
+  const upperCase = text.toUpperCase();
+  
+  return `${lowerCase}-${upperCase}`;
 }
 
 // DO not change the code below
